62b016f78c75948b11ca94e9f9677b968e4b2660
RAMTEJAKUDUPUDI <104918813+RAMTEJAKUDUPUDI@users.noreply.github.com>
2023-08-09 20:52:38 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name="ram"
+name="teja"
+name="nani"
+print(name)
+
+
+# In[3]:
+
+
+ram_age=sai_age=tom_age = 19
+print("ram's age =",ram_age)
+print("sai's age =",sai_age)
+print("tom's =",tom_age)
+
+
+# In[13]:
+
+
+#implicit convertion
+price=4.5
+no_of_items=18
+total_prize=0
+total_prize=(price*no_of_items)
+print(total_prize)
+
+
+# In[15]:
+
+
+#Explict converction
+price=25
+tax=2.5
+total_price=price+float(tax)
+print(total_price)
+
+
+# In[21]:
+
+
+unit_price=52.6
+str(unit_price)
+type(unit_price)
+
+
+
+# In[20]:
+
+
+n=1233
+str(n)
+
+
+# In[22]:
+
+
+hasspeed,isLivingthings =1,0
+bool(hasspeed)
+
+
+# In[24]:
+
+
+side=20
+area_square=side**2
+print(area_square)
+
+
+# In[25]:
+
+
+cost=200
+quantity=15
+price_perunit=cost/quantity
+print(price_perunit)
+
+
+# In[26]:
+
+
+cost=200
+quantity=15
+price_perunit=cost%quantity
+print(price_perunit)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
